e11906106486884e4a02e89c6ec5fbe3479bf20c
Aboullah Balde <abdoullahbalde@gmail.com>
2023-08-09 22:38:14 +0200

    commit string php

---
+++ 08_fonctions_strings.php
@@ -0,0 +1,32 @@
+<?php
+
+$string = "saluts les gens.....";
+// print_r(strlen($string));
+
+// trouver l'emplacement du u
+// $s =   strpos($string, 's');
+
+// mettre ne majuscule
+// $s = strtoupper($string);
+
+
+// inverser les mots
+
+// print_r(strrev($string));
+
+// remplacer des mots
+echo '<br>';
+
+// $r = str_replace('salut', 'bonjour', $string);
+
+// print_r($r);
+
+// supprimer des elements
+// $s = substr($string, 6);
+
+// print_r($s);
+
+
+$ht = '<h1> Helo worlds </h1>';
+
+echo $ht;
